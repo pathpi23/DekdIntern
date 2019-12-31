ae9090a443f3e6185e348b7d728d91d575f343d6
pathpi23 <pathpi23@users.noreply.github.com>
2019-12-31 21:07:19 +0700

    format

--- slide.js
+++ slide.js
@@ -1,96 +1,92 @@
+var picadd = ["img/pic1.jpg", "img/pic2.jpg", "img/pic3.jpg", "img/pic4.jpg"];
 
+//funtion find index of current picture
 
-        var picadd = ["img/pic1.jpg", "img/pic2.jpg", "img/pic3.jpg", "img/pic4.jpg"];
+function findInd(x) {
+  console.log("1");
 
-        function findind(x) {
-            console.log("1");
+  var current = document.getElementsByClassName("cur")[0].src;
+  var curstr = current.split("/");
+  current = "img/" + curstr[curstr.length - 1];
+  ind = picadd.findIndex(picadd => picadd == current);
+  if (x == 1) {
+    if (ind != picadd.length - 1) {
+      fade(ind + 1);
+    } else {
+      fade(0);
+    }
+  } else {
+    if (ind != 0) {
+      fade(ind - 1);
+    } else {
+      fade(picadd.length - 1);
+    }
+  }
 
-            var current = document.getElementsByClassName("cur")[0].src;
-            var curstr = current.split("/");
-            current = "img/" + curstr[curstr.length - 1];
-            ind = picadd.findIndex(picadd => picadd == current);
-            if (x == 1) {
-                if (ind != picadd.length - 1) {
-                    fade(ind + 1);
-                }
-                else {
-                    fade(0);
-                }
-            }
-            else {
-                if (ind != 0) {
-                    fade(ind - 1);
-                }
-                else {
-                    fade(picadd.length - 1);
-                }
-            }
+  console.log(ind);
+}
 
-            console.log(ind);
-        }
+//function fade
 
+function fade(n) {
+  document.getElementById("ss").classList.add("chkslide");
+  setTimeout(function() {
+    pics(n);
+  }, 500);
 
-        function fade(n) {
-            document.getElementById("ss").classList.add('chkslide');
-            setTimeout(function () { pics(n) }, 500);
+  setTimeout(function() {
+    document.getElementById("ss").classList.remove("chkslide");
+  }, 1000);
+}
+//function change picture
+function pics(n) {
+  var pict = document.getElementsByClassName("picture");
+  var dot = document.getElementsByClassName("dot");
 
-            setTimeout(function () { document.getElementById("ss").classList.remove('chkslide'); }, 1000);
-        }
-        function pics(n) {
-            var pict = document.getElementsByClassName("picture");
-            var dot = document.getElementsByClassName("dot");
+  for (i = 0; i < dot.length; i++) {
+    if (i == n) {
+      dot[i].classList.add("active");
+    } else {
+      dot[i].classList.remove("active");
+    }
+  }
+  if (picadd.length >= 3) {
+    console.log("check");
+    if (n != picadd.length - 1 && n != 0) {
+      console.log(picadd[n]);
 
-            for(i=0;i<dot.length;i++){
-                if(i==n){
-                    dot[i].classList.add('active');
-                }
-                else{
-                    dot[i].classList.remove('active');
-                }
-            }
-            if (picadd.length >= 3) {
-                console.log("check");
-                if (n != picadd.length - 1 && n != 0) {
-                    console.log(picadd[n]);
-
-                    pict[0].src = picadd[n - 1];
-                    pict[1].src = picadd[n];
-                    pict[2].src = picadd[n + 1];
-                }
-                else if (n == picadd.length - 1)//last pic
-                {
-                    pict[0].src = picadd[n - 1];
-                    pict[1].src = picadd[n];
-                    pict[2].src = picadd[0];
-                }
-                else {//first pic
-                    pict[0].src = picadd[picadd.length - 1];
-                    pict[1].src = picadd[n];
-                    pict[2].src = picadd[n + 1];
-                }
-
-            }
-            else {
-                if (picadd.length == 1) {
-                    pict[0].src = "img/nopic.jpg";
-                    pict[1].src = picadd[0];
-                    pict[2].src = "img/nopic.jpg";
-                }
-                if (picadd.length == 2) {
-                    if (n == pict.length - 1) {
-                        pict[0].src = picadd[0];
-                        pict[1].src = picadd[1];
-                        pict[2].src = picadd[0];
-                    }
-                    else {
-                        pict[0].src = picadd[1];
-                        pict[1].src = picadd[0];
-                        pict[2].src = picadd[1];
-                    }
-                }
-            }
-
-
-        }
-        pics(0);
-
+      pict[0].src = picadd[n - 1];
+      pict[1].src = picadd[n];
+      pict[2].src = picadd[n + 1];
+    } else if (n == picadd.length - 1) {
+      //last pic
+      pict[0].src = picadd[n - 1];
+      pict[1].src = picadd[n];
+      pict[2].src = picadd[0];
+    } else {
+      //first pic
+      pict[0].src = picadd[picadd.length - 1];
+      pict[1].src = picadd[n];
+      pict[2].src = picadd[n + 1];
+    }
+  } else {
+    if (picadd.length == 1) {
+      pict[0].src = "img/nopic.jpg";
+      pict[1].src = picadd[0];
+      pict[2].src = "img/nopic.jpg";
+    }
+    if (picadd.length == 2) {
+      if (n == pict.length - 1) {
+        pict[0].src = picadd[0];
+        pict[1].src = picadd[1];
+        pict[2].src = picadd[0];
+      } else {
+        pict[0].src = picadd[1];
+        pict[1].src = picadd[0];
+        pict[2].src = picadd[1];
+      }
+    }
+  }
+}
+//initial
+pics(0);
